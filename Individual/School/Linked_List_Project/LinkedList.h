f51085058a52c59080c06e217fe29aa8564db7dc
wavesdog470 <102263445+wavesdog470@users.noreply.github.com>
2022-04-06 16:52:32 -0400

    Update LinkedList.h

--- Individual/School/Linked_List_Project/LinkedList.h
+++ Individual/School/Linked_List_Project/LinkedList.h
@@ -37,44 +37,6 @@ private:
 	Node* _head = nullptr;
 	unsigned int _size = 0;
 public:
-	//behaviors
-	//void PrintForward() const;
-	/*void PrintReverse() const;
-	void PrintForwardRecursive(const Node* node) const;
-	void PrintReverseRecursive(const Node* node) const;
-	//getters
-	unsigned int NodeCount() const;
-	void FindAll(vector<Node*>& outData, const T& value) const;
-	const Node* Find(const T& data) const;
-	Node* Find(const T& data);
-	Node* GetNode(unsigned int index);
-	const Node* GetNode(unsigned int index) const;
-	Node* Head();
-	const Node* Head() const;
-	Node* Tail();
-	const Node* Tail() const;
-	//insert 
-	void AddTail(const T& data);
-	void AddNodesHead(const T* data, unsigned int count);
-	void AddNodesTail(const T* data, unsigned int count);
-	void InsertAfter(Node* node, const T& data);
-	void InsertBefore(Node* node, const T& data);
-	void InsertAt(const T& data, unsigned int index);
-	//Remove
-	bool RemoveHead();
-	bool RemoveTail();
-	unsigned int Remove(const T& data);
-	bool RemoveAt(unsigned int index);
-	void Clear();
-	*/
-	//Operators
-	//const T& operator[](unsigned int index) const;
-	//T& operator[](unsigned int index);
-	// bool operator ==(const LinkedList<T>& rhs);
-	//LinkedList<T>& operator=(const LinkedList<T>& rhs);
-		//construction stuff
-	//LinkedList();
-	//LinkedList(const LinkedList<T>& list);
 	~LinkedList() {
 		Clear();
 	}
